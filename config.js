56e27d3cd8cac6ff2c95bd416f03b86d9610a1b1
FarhanAli690 <farhanali28690@gmail.com>
2024-05-30 05:22:07 +0500

    Update config.js

--- config.js
+++ config.js
@@ -5,15 +5,15 @@ import fs from 'fs'
 
 //Owner Numbers 
 global.owner = [
-  ['923135673658', 'wasi', false],
+  ['923471728690', 'Farhan', false],
   [''], 
   [''],
 ]
 
 //global.pairingNumber = "" //put your bot number here
 global.mods = ['923305673658'] 
-global.prems = ['923305673658', '923305673658', '923305673658']
-global.allowed = ['923305673658']
+global.prems = ['923471728690', '923435030807', '923305673658']
+global.allowed = ['923471728690']
 global.keysZens = ['c2459db922', '37CC845916', '6fb0eff124']
 global.keysxxx = keysZens[Math.floor(keysZens.length * Math.random())]
 global.keysxteammm = ['29d4b59a4aa687ca', '5LTV57azwaid7dXfz5fzJu', 'cb15ed422c71a2fb', '5bd33b276d41d6b4', 'HIRO', 'kurrxd09', 'ebb6251cc00f9c63']
